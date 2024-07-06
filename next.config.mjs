ccea535be9ca25f36d63e702ca24cfdff54478e2
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-07-06 13:14:27 +0700

    update config next js for cors allowing all

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { headers } from 'next/headers';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
@@ -13,24 +11,31 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/api/*',
+                // Routes this applies to
+                source: "/api/(.*)",
+                // Headers
                 headers: [
+                    // Allow for specific domains to have access or * for all
                     {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                        // DOES NOT WORK
+                        // value: process.env.ALLOWED_ORIGIN,
                     },
+                    // Allows for specific methods accepted
                     {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,OPTIONS',
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
                     },
+                    // Allows for specific headers accepted (These are a few standard ones)
                     {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'X-Requested-With, Content-Type, Accept',
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
                     },
-                ]
-            }
-        ]
-    }
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
