80248f656c808d035f3303d629bfc10e01d151f6
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-07-06 12:44:51 +0700

    allow all cors

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
             {
                 source: '/api/docs',
                 destination: '/api/docs',
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                ]
             },
         ];
     },
