9f43a408a58b709b8c1910673e6abfdfff24f71a
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-07-11 14:20:52 +0700

    add task apis

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -2,35 +2,110 @@ import { NextRequest, NextResponse } from "next/server";
 
 /**
  * @swagger
- * /api/reminder:
+ * /api/task/{id}:
  *   get:
- *     summary: Retrieve a list of reminders
- *     tags: 
- *       - Reminders
+ *     summary: Retrieve a task
+ *     description: Retrieve a task with its details
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - name: id
+ *         in: path
  *     responses:
  *       200:
- *         description: A list of reminders
+ *         description: A task
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                   name:
- *                     type: string
- *                   time:
- *                     type: string
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The task ID
+ *                 name:
+ *                   type: string
+ *                   description: The name of the task
+ *                 description:
+ *                   type: string
+ *                   description: The description of the task
+ *                 completed:
+ *                   type: boolean
+ *                   description: The completion status of the task
  */
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const task = {
+        id: id,
+        name: "Membersihkan rumah",
+        description: "membersihkan rumah jam 12 pagi",
+        completed: false,
+    }
+    return NextResponse.json(task, { status: 200 });
+}
 
-export async function GET(request: NextRequest) {
-    const reminders = [
-        {
-            id: 1,
-            name: "Planning"
-        }
-    ];
-    return NextResponse.json(reminders, { status: 200 });
+/**
+ * @swagger
+ * /api/task/{id}:
+ *   delete:
+ *     summary: Delete a task
+ *     description: Delete a task by its ID
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *     responses:
+ *       200:
+ *         description: A task
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ */
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
+    return NextResponse.json({ id }, { status: 200 });
+}
+
+// update the task
+/**
+ * @swagger
+ * /api/task/{id}:
+ *   put:
+ *     summary: Update a task
+ *     description: Update a task by its ID
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *     responses:
+ *       200:
+ *         description: A task
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 completed:
+ *                   type: boolean
+ */
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const task = {
+        id: id,
+        name: "Membersihkan rumah",
+        description: "membersihkan rumah jam 12 pagi",
+        completed: true,
+    }
+    return NextResponse.json(task, { status: 200 });
 }
