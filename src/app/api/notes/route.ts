637cfac7bee38606d7da5f0a0dcb0240040a01cb
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-07-11 13:59:49 +0700

    create post notes

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -6,6 +6,8 @@ import { NextRequest, NextResponse } from 'next/server';
  *   get:
  *     summary: Retrieve a list of notes
  *     description: Retrieve a list of notes with their details
+ *     tags:
+ *       - Notes
  *     responses:
  *       200:
  *         description: A list of notes
@@ -46,4 +48,74 @@ export async function GET(request: NextRequest) {
         },
     ];
     return NextResponse.json(notes, { status: 200 });
+}
+
+/**
+ * @swagger
+ * /api/notes:
+ *   post:
+ *     summary: Create a new note
+ *     description: Create a new note with the given name and description
+ *     tags:
+ *       - Notes
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the note
+ *               description:
+ *                 type: string
+ *                 description: The description of the note
+ *     responses:
+ *       201:
+ *         description: Note created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The response message
+ *                 note:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The note ID
+ *                     date:
+ *                       type: string
+ *                       description: The date of the note
+ *                     name:
+ *                       type: string
+ *                       description: The name of the note
+ *                     description:
+ *                       type: string
+ *                       description: The description of the note
+ */
+export async function POST(request: NextRequest) {
+    // validate request body
+    const body = await request.json();
+    if (!body.name || !body.description) {
+        return NextResponse.json({ message: "Name and description are required" }, { status: 400 });
+    }
+
+    const response_success = {
+        message: "Note created successfully",
+        note: {
+            id: 3,
+            date: "24 May 2024, 06.00 PM",
+            name: body.name,
+            description: body.description,
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+        },
+    };
+
+    return NextResponse.json(response_success, { status: 201 });
 }
